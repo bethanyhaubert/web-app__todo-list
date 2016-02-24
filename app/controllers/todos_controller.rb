1a95898b4b9d1898497448f992635a5ef269ef5e
Beth Haubert <bethany.haubert@gmail.com>
2016-02-24 14:56:06 -0600

    can't figure out why profile not loading

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,6 @@
 MyApp.get "/" do
+	@todo_list =  Todo.all
+	@current_user = User.find_by_id(session["user_id"])
   erb :"index"
 end
 
@@ -34,17 +36,22 @@ MyApp.post "/add_todo" do
 	end
 end
 
+MyApp.get "/todo_info/:todo.id" do
+	@todo = Todo.find_by_id(params[:todo_id])
+	erb :"todo_info/#{@todo.id}"
+end
+
 MyApp.post "/edit_todo/:todo_id" do
 	@current_user = User.find_by_id(session["user_id"])
 	if @current_user != nil
 	@todo = Todo.find(params[:todo_id])
-	todo.assign_attributes({title: params['title'], description: params['description']})
-	todo.user_id = @current_user.id
-		if todo.is_valid == true
-			todo.save
-	  		redirect :"todo_info/#{todo.id}"
+	@todo.assign_attributes({title: params['title'], description: params['description']})
+	@todo.user_id = @current_user.id
+		if @todo.is_valid == true
+			@todo.save
+	  		redirect :"todo_info/#{@todo.id}"
 		else
-			@error_object = todo
+			@error_object = @todo
 			erb :"error"
 		end
 	else
