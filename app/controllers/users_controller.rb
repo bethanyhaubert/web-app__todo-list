a3ae052dc55e16307c2821bbad93aed0ed6f548e
Beth Haubert <bethany.haubert@gmail.com>
2016-02-24 10:54:56 -0600

    create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,1 +1,18 @@
-# This controller is for all the CRUD operations related to a User.
+MyApp.get "/users/create_user" do
+  erb :"users/create_user"
+end
+
+MyApp.post "/add_user" do
+	@user = User.new
+	@user.name = params[:name]
+	@user.email = params[:email]
+	@user.password = params[:password]
+	# if @competitor.is_valid == true
+	# 	@competitor.save
+	# 	redirect :"profile/#{@competitor.id}"
+	# else
+	# 	@error_object = @competitor
+	# 	erb :"error"
+	# end
+	erb :"success"
+end
