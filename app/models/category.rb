62c92b831377fbacacd3562e82d76ddf0220d63b
Beth Haubert <bethany.haubert@gmail.com>
2016-02-25 14:29:22 -0600

    added category views

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,31 @@
 class Category < ActiveRecord::Base
+
+	#Returns @errors
+  	def get_errors
+    	return @errors
+ 	end
+
+  	#Adds errors to Hash
+  	#
+  	#Returns Hash
+  	def set_errors
+    	@errors = []
+
+    	if self.name == ""
+      	@errors << "Name cannot be blank"
+    	end
+  	end
+
+  # Checks if the record is valid.
+  # 
+  # Returns Boolean.
+  	def is_valid
+    	self.set_errors
+    	if @errors.length > 0
+      	return false
+    	else
+      	return true
+    	end
+  	end
+end
 end
